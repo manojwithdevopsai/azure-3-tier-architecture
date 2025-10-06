64c67dd4f70c49b87ae29217fc99f7f9ae7328b1
Manoj Savukar <92372003+manojwithdevopsai@users.noreply.github.com>
2025-10-06 23:03:02 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "savukar" 
+    storage_account_name = "manoj" 
     container_name       = "tfstate" 
     key                  = "3tier/dev.terraform.tfstate"
   }
